093807aa74d953a3a680072c388655e49b5c8a4d
RafaelJuliano <rafael.j.ferreira@hotmail.com>
2022-07-11 19:31:19 -0300

    fix import categories

--- src/modules/cars/useCases/importCategory/importCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/importCategoryUseCase.ts
@@ -1,11 +1,57 @@
-import { ImportCategoryUseCase } from "./ImportCategoryController"
+import fs from 'fs'
+import { Parser } from 'csv-parse'
+import { CategoryRepository } from '../../repositories/implementations/CategoryRepository'
 
-export class ImportCategoryController {
-    constructor(private importCategoryUseCase: ImportCategoryUseCase) { }
+interface IImportCategory {
+    name: string
+    description: string
+}
 
-    handle(request, response) {
-        const { file } = request
-        this.importCategoryUseCase.execute(file)
-        response.send()
+export class ImportCategoryUseCase {
+
+    constructor(private categoryRepository: CategoryRepository) { }
+
+    loadCategories(file: Express.Multer.File): Promise<IImportCategory[]> {
+        return new Promise((resolve, reject) => {
+            const categories: IImportCategory[] = []
+            const stream = fs.createReadStream(file.path)
+
+            const parseFile = new Parser({
+                delimiter: ',',
+            })
+
+            stream.pipe(parseFile)
+
+            parseFile.on("data", async line => {
+                const [name, description] = line
+                categories.push({
+                    name,
+                    description
+                })
+            })
+                .on("end", () => {
+                    resolve(categories)
+                })
+                .on("error", (err) => {
+                    reject(err)
+                })
+        })
+    }
+
+    async execute(file: Express.Multer.File): Promise<void> {
+        const categories = await this.loadCategories(file)
+
+        categories.map(async category => {
+            const { name, description } = category
+
+            const categoryExists = this.categoryRepository.findByName(name)
+
+            if (!categoryExists) {
+                this.categoryRepository.create({
+                    name,
+                    description
+                })
+            }
+        })
     }
 }
