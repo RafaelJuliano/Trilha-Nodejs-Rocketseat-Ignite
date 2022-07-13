5ef4f6477e655c9f84f73c6dbc0a2d07a1b887dc
RafaelJuliano <rafael.j.ferreira@hotmail.com>
2022-07-13 07:10:12 -0300

    created CreateUserController

---
+++ src/modules/users/useCases/createUser/CreateUserController.ts
@@ -0,0 +1,28 @@
+import { Request, Response } from 'express'
+import { container } from "tsyringe";
+import { CreateUserUseCase } from "./CreateUserUseCase";
+
+
+export class CreateUserController {
+
+    async handle(request: Request, response: Response): Promise<Response> {
+
+        const { name, username, email, driver_license, password } = request.body
+
+        const createUserUseCase = container.resolve(CreateUserUseCase)
+
+        try {
+            await createUserUseCase.execute({
+                name,
+                username,
+                email,
+                driver_license,
+                password
+            })
+            return response.status(201).send();
+        } catch (error) {
+            return response.status(500).send()
+        }
+
+    }
+}
